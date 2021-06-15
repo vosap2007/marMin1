975a636690d9e6fb09e22642f51417434ab934a7
Volodymyr Udovychenko <vosap2007@i.ua>
2021-06-15 12:36:31 +0300

    дописывает js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+function slidesPlugin(activeSlide = 0) {
 const slides = document.querySelectorAll('.slide');
+
+slides[activeSlide].classList.add('active');
 
 for (const slide of slides) {
    slide.addEventListener('click', () => {
@@ -13,3 +16,7 @@ function clearActiveClasses() {
         slide.classList.remove('active')
     })
 }
+}
+
+slidesPlugin(2);
+
